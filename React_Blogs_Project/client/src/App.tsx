2f5d125900fb045e3e89bbe0605b85a98a648eff
OmriAharonov <omri832@gmail.com>
2022-06-20 00:43:28 +0300

    blog start

---
+++ React_Blogs_Project/client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './view/styles/global.scss';
+import axios from 'axios';
+
+function App() {
+  return (
+    <div className="blogApp">
+   
+    </div>
+  );
+}
+
+export default App;
